3f0f7852d8d4e1bd691f01776402e1a59f9f2503
王云海 <adoin@qq.com>
2024-08-29 13:23:54 +0800

    feat: update doc

--- examples/App.vue
+++ examples/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="{ night: isNight }" style="height: 100vh; padding: 1em">
-    <day-night v-model="isNight" />
+    <day-night v-model="isNight" class-name="absolute left-0 top-0 scale-0.4" />
     <div>现在是{{ isNight ? '晚上' : '白天' }}</div>
   </div>
 </template>
@@ -14,5 +14,17 @@
   .night {
     background-color: #000;
     color: #fff;
+  }
+  .absolute {
+    position: absolute;
+  }
+  .left-0 {
+    left: 0;
+  }
+  .top-0 {
+    top: 0;
+  }
+  .scale-0\.4 {
+    transform: scale(0.4);
   }
 </style>
